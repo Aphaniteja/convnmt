a8eb0ef38e7a60078710647ebf86b84636bbae11
aphaniteja <teja.phani@gmail.com>
2019-04-18 15:03:16 +0530

    download files

---
+++ download.sh
@@ -0,0 +1,9 @@
+wget https://www.dropbox.com/s/z4st1e85v8w8w68/en_train.pkl?dl=0
+wget https://www.dropbox.com/s/fn9umbt17bax9j2/en_valid.pkl?dl=0
+wget https://www.dropbox.com/s/ep2v5xgaopujexe/fr_train.pkl?dl=0
+wget https://www.dropbox.com/s/2lqsdrrdcle8mie/fr_valid.pkl?dl=0
+
+mv en_valid.pkl?dl=0 en_valid.pkl
+mv en_train.pkl?dl=0 en_train.pkl
+mv fr_valid.pkl?dl=0 fr_valid.pkl
+mv fr_train.pkl?dl=0 fr_train.pkl
